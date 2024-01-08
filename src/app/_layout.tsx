642a8af25b4ee34ada7806b28ce0acefa1d95a0e
shunkominato <shun19871001@gmail.com>
2024-01-08 09:02:35 +0900

    layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
+import { NativeBaseProvider, extendTheme } from 'native-base';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 
@@ -20,7 +21,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    MPLUSRounded: require('../../assets/fonts/MPLUSRounded1c-Bold.ttf'),
     ...FontAwesome.font,
   });
 
@@ -44,13 +45,32 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
+  const theme = extendTheme({
+    colors: {
+      // Add new color
+      primary: {
+        50: 'rose.50',
+        100: 'rose.100',
+        200: 'rose.200',
+        300: 'rose.300',
+        400: 'rose.400',
+        500: 'rose.500',
+        600: 'rose.600',
+        700: 'rose.700',
+        800: 'rose.800',
+        900: 'rose.900',
+      },
+    },
+  });
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <NativeBaseProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </NativeBaseProvider>
     </ThemeProvider>
   );
 }
